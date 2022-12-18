9da47284830e661010604beaa06bc474f8278f45
yhuzued <yusufmait@gmail.com>
2022-12-18 11:25:35 +0700

    Create image display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    flickr = Flickr.new ENV['flickr_key'], ENV['flcikr_secret']
+    begin
+      id = params[:user_id]
+      @photos = flickr.photos.search(user_id: id)
+    rescue
+      render static_pages_home_path, notice: "Something wrong"
+    end
   end
 end
